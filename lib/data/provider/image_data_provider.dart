69a018446e6e1fb4c6f64908811142a10eaa5dbe
LbnMahs <labanmahs2002@gmail.com>
2024-02-17 07:42:28 +0300

    Installing additional packages(flutter_bloc + http)

--- lib/data/provider/image_data_provider.dart
+++ lib/data/provider/image_data_provider.dart
@@ -1,3 +1,9 @@
 class ImageDataRepository {
-  
+  // Future<List<String>> getImagesByCollection(String collection) async {
+  //   try{
+
+  //   } catch(e) {
+  //     throw e.toString();
+  //   }
+  // }
 }
