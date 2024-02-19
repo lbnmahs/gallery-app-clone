1a8b6fbe7c966eaaa78698a3c2650acc5d379048
LbnMahs <labanmahs2002@gmail.com>
2024-02-19 17:48:43 +0300

    getting the API and putting it in a data provider

--- lib/data/provider/image_data_provider.dart
+++ lib/data/provider/image_data_provider.dart
@@ -1,9 +1,20 @@
-class ImageDataRepository {
-  // Future<List<String>> getImagesByCollection(String collection) async {
-  //   try{
+import 'dart:convert';
 
-  //   } catch(e) {
-  //     throw e.toString();
-  //   }
-  // }
+import 'package:gallery_app/secrets.dart';
+import 'package:http/http.dart' as http;
+
+class ImageDataRepository {
+  Future<List<String>> getImagesByCollection(String collection) async {
+    try{
+      final res = await http.get(
+        Uri.parse('https://api.unsplash.com/photos/random?count=5&query=$collection'),
+        headers: {'Authorization': 'Client-ID $client_id'}
+      );
+      final List<String> images = json.decode(res.body);
+
+      return images;
+    } catch(e) {
+      throw e.toString();
+    }
+  }
 }
