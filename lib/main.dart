5b2a24ecbc41967a5d8019ec42ace3553d57f9d0
LbnMahs <labanmahs2002@gmail.com>
2024-02-23 21:06:24 +0300

    Wrapping the main entry point with repository provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:gallery_app/screens/albums.screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'package:gallery_app/bloc/images_bloc.dart';
+import 'package:gallery_app/data/provider/image_data_provider.dart';
+import 'package:gallery_app/data/repository/image_repository.dart';
+import 'package:gallery_app/screens/albums.screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,18 +15,20 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Gallery App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          brightness: Brightness.dark,
-          seedColor: Colors.deepPurple
+    return RepositoryProvider(
+      create: (context) => ImageRepository(ImageDataProvider()),
+      child: BlocProvider(
+        create: (context) => ImagesBloc(context.read<ImageRepository>()),
+        child: MaterialApp(
+          title: 'Gallery App',
+          theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(
+                  brightness: Brightness.dark, seedColor: Colors.deepPurple),
+              useMaterial3: true,
+              textTheme: GoogleFonts.robotoTextTheme()),
+          home: const AlbumScreen(),
         ),
-        useMaterial3: true,
-        textTheme: GoogleFonts.robotoTextTheme()
       ),
-      home: const AlbumScreen(),
     );
   }
 }
-
